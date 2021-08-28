9132e45645db208907ef18cd828cc51f963e53d8
Alastair Rosewood <37487746+alastairtechnologies@users.noreply.github.com>
2021-08-28 13:36:10 -0700

    init commit

---
+++ data/projectsData.js
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'Cloudflare Workers',
+    description: `What if deploying code to the edge could be as simple as typing publish? Well, that's what's great about Cloudflare Workers! They deploy fast, run faster, and are available around the world, so you are never too far away from the websites you need the most. In fact, this website is built on Workers!`,
+    imgSrc: '/static/images/workers.jpeg',
+    href: 'https://workers.cloudflare.com',
+  },
+  {
+    title: 'Discord Developing',
+    description: `Imagine a place, where you could chat with friends, while having fun, while getting work done... Well, that's Discord! And even better, you can build stuff on Discord too! It's really fun, and easy to get started!`,
+    imgSrc: '/static/images/discord.jpg',
+    href: 'https://discord.com/developers',
+  },
+]
+
+export default projectsData
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+}
